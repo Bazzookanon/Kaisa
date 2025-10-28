d855809fd6d9d288e6292b37d9743c0b934d7df0
Bazzookanon <75884351+Bazzookanon@users.noreply.github.com>
2025-10-28 18:43:43 +0800

    update 28102025

--- routes/web.php
+++ routes/web.php
@@ -10,22 +10,25 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-
-// Server info page (shows uptime, memory, disk)
-
 |
 */
- Route::get('/server', [\App\Http\Controllers\ServerController::class, 'index'])->name('server.info');
+
+// Server info page (shows uptime, memory, disk)
+Route::get('/server', [\App\Http\Controllers\ServerController::class, 'index'])->name('server.info');
+
 Route::get('/', function () {
-    return redirect()->route('login.page');
+    return redirect()->route('login');
 })->name('home');
 
 // Secure Login routes
 Route::middleware(['web', App\Http\Middleware\LoginSecurityMiddleware::class])->group(function () {
     Route::get('/login', [App\Http\Controllers\Auth\SecureLoginController::class, 'showLoginForm'])->name('login');
-    Route::post('/login', [App\Http\Controllers\Auth\SecureLoginController::class, 'login'])->name('login.page');;
-    Route::post('/logout', [App\Http\Controllers\Auth\SecureLoginController::class, 'logout'])->name('logout');
+    Route::post('/login', [App\Http\Controllers\Auth\SecureLoginController::class, 'login'])->name('login.submit');
 });
+
+// Logout routes (separate from security middleware to avoid issues)
+Route::post('/logout', [App\Http\Controllers\Auth\SecureLoginController::class, 'logout'])->name('logout')->middleware('web');
+Route::get('/logout', [App\Http\Controllers\Auth\SecureLoginController::class, 'logout'])->name('logout.get')->middleware('web');
 
 // Registration routes
 Route::middleware(['web', App\Http\Middleware\LoginSecurityMiddleware::class])->group(function () {
@@ -38,6 +41,7 @@ Route::middleware(['web', App\Http\Middleware\LoginSecurityMiddleware::class])->
 // })->name('category.index');
 
 Route::resource('products', \App\Http\Controllers\ProductController::class);
+Route::get('/list', [\App\Http\Controllers\ProductController::class, 'list'])->name('products.list');
 Route::resource('categories', \App\Http\Controllers\CategoryController::class);
 Route::post('/products/search', [\App\Http\Controllers\ProductController::class, 'searchProduct'])->name('products.search');
 Route::post('/categories/search', [\App\Http\Controllers\CategoryController::class, 'searchCategory'])->name('categories.search');
